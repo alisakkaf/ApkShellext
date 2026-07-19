cec8809f204ef2410cda570357765ce3d68c94eb
alisakkaf <alisakkaf010@gmail.com>
2026-07-19 23:56:34 +0300

    Add InheritTerm.cs

---
+++ ApkShellext/SVG/Source/External/ExCSS/Model/Values/InheritTerm.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Svg.ExCSS
+{
+    public class InheritTerm : Term
+    {
+        internal InheritTerm()
+        {
+        }
+
+        public override string ToString()
+        {
+            return "inherit";
+        }
+    }
+}
+
