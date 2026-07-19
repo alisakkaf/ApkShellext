d780daebe875d1b3aaa505d953ddf55f03effd08
alisakkaf <alisakkaf010@gmail.com>
2026-07-19 23:55:04 +0300

    Add AssemblyInfo.cs

---
+++ ApkShellext/SharpZipLib/samples/cs/CreateZipFile/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("CreateZipFile")]
+[assembly: AssemblyDescription("Free C# IDE")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("A SharpDevelop sample")]
+[assembly: AssemblyCopyright("Mike Krueger 2000")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
