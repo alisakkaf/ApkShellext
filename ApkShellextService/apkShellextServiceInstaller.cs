25d0156d65b681e7a492e3835d90a947a0e6839f
alisakkaf <alisakkaf010@gmail.com>
2026-07-20 00:01:54 +0300

    Add apkShellextServiceInstaller.cs

---
+++ ApkShellextService/apkShellextServiceInstaller.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace ApkShellext {
+    [RunInstaller(true)]
+    public class apkShellextServiceInstaller : Installer {
+
+        public apkShellextServiceInstaller() {
+            ServiceProcessInstaller serviceProcessInstaller =
+                               new ServiceProcessInstaller();
+            ServiceInstaller serviceInstaller = new ServiceInstaller();
+
+            //# Service Account Information
+            serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            serviceProcessInstaller.Username = null;
+            serviceProcessInstaller.Password = null;
+
+            //# Service Information
+            serviceInstaller.DisplayName = "ApkShellext Service";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            //# This must be identical to the WindowsService.ServiceBase name
+            //# set in the constructor of WindowsService.cs
+            serviceInstaller.ServiceName = "ApkShellext Service";
+
+            this.Installers.Add(serviceProcessInstaller);
+            this.Installers.Add(serviceInstaller);
+        }
+    }
+}
