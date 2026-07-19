a9c8e3975e48f9641fc98b2a98baa4278d221ba3
alisakkaf <alisakkaf010@gmail.com>
2026-07-19 23:56:45 +0300

    Add ISvgBoundable.cs

---
+++ ApkShellext/SVG/Source/Painting/ISvgBoundable.cs
@@ -0,0 +1,22 @@
+using System.Drawing;
+
+namespace Svg
+{
+    public interface ISvgBoundable
+    {
+        PointF Location
+        {
+            get;
+        }
+
+        SizeF Size
+        {
+            get;
+        }
+
+        RectangleF Bounds
+        {
+            get;
+        }
+    }
+}
