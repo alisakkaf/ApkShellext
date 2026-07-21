47b74b254505c9dc9b5ce19749c96d3279270b07
alisakkaf <alisakkaf010@gmail.com>
2026-07-21 21:09:54 +0300

    Update V1.1.0 Uninstall Servers.bat"

--- ApkShellext/SharpShell/Tests/Uninstall Servers.bat
+++
@@ -1,6 +0,0 @@
-..\build\tools\srm.exe uninstall "..\build\samples\countlinesextension.dll" 
-..\build\tools\srm.exe uninstall "..\build\samples\filetimespropertysheet.dll"
-..\build\tools\srm.exe uninstall "..\build\samples\ReadOnlyFileIconOverlayHandler.dll"
-..\build\tools\srm.exe uninstall "..\build\samples\TxtThumbnailHandler.dll"
-..\build\tools\srm.exe uninstall "..\build\samples\IconPreviewHandler.dll" -codebase
-pause
